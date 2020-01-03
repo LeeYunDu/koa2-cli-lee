97c56eae47fb1a2c122d47ce4aabac0177adffc2
LeeYunDu <492107910@qq.com>
2020-01-03 10:33:31 +0800

    测试提交6

--- src/app/controllers/test_controller.ts
+++ src/app/controllers/test_controller.ts
@@ -10,17 +10,7 @@ export const testPost1 = async (ctx: any) => {
     data:new Date()
   };
 };
-export const testPost2 = async (ctx: any) => {
-  ctx.body = {
-    message: "this is test Post",
-    data:new Date()
-  };
-};
-export const testPost3 = async (ctx: any) => {
-  ctx.body = {
-    message: "this is test Post",
-    data:new Date()
-  };
-};
+
+
 
 
